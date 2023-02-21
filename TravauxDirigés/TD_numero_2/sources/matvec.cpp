f32dea9b571be49090f94040c9a1b211ee996a38
Tao Guinot <tao.guinot@ensta-paris.fr>
2023-02-21 10:07:34 +0100

    21-02-23

--- TravauxDirigés/TD_numero_2/sources/matvec.cpp
+++ TravauxDirigés/TD_numero_2/sources/matvec.cpp
@@ -2,6 +2,9 @@
 # include <cassert>
 # include <vector>
 # include <iostream>
+#include <mpi.h>
+
+// To run : mpirun --oversubscribe  -np  8 ./matvec.exe
 
 // ---------------------------------------------------------------------
 class Matrix : public std::vector<double>
@@ -96,15 +99,130 @@ Matrix::Matrix( int nrows, int ncols ) : m_nrows(nrows), m_ncols(ncols),
     }    
 }
 // =====================================================================
+void prod(const Matrix& A, int iPack,std::vector<double> u, double* buffer, int N){
+
+    
+    assert(u.size()==unsigned(N));
+    for(int i = 0; i<N; ++i){
+        buffer[i]+= A(i, iPack)*u[iPack];
+    }  
+}
+
+std::vector<double> somme(std::vector<double> result, std::vector<double> buffer, int N){
+    std::vector<double> v(N);
+    for(int i =0; i<N; ++i){
+        v[i] = result[i]+buffer[i];
+    }
+    return v;
+}
+
+// =====================================================================
 int main( int nargs, char* argv[] )
 {
     const int N = 120;
     Matrix A(N);
-    std::cout  << "A : " << A << std::endl;
     std::vector<double> u( N );
     for ( int i = 0; i < N; ++i ) u[i] = i+1;
-    std::cout << " u : " << u << std::endl;
-    std::vector<double> v = A*u;
-    std::cout << "A.u = " << v << std::endl;
+    
+
+    // Maitre esclave :
+    int rank, nbp;
+    MPI_Init(&nargs, &argv);
+    MPI_Comm globComm;
+    MPI_Comm_dup(MPI_COMM_WORLD, &globComm);
+    MPI_Comm_rank(globComm, &rank);
+    MPI_Comm_size(globComm, &nbp);
+
+    
+
+    std::vector<double> buffer(N);
+    std::vector<double> result(N);
+
+    // Maitre
+    if(rank==0){
+        //iPack est le numero de colonne de A à faire intervenir dans le calcul, iPack in [0, N]
+        int iPack;
+        for(int i=1; i<nbp; i++){
+            iPack = i-1;
+            MPI_Send(&iPack, 1, MPI_INT, i, 101, globComm);
+        }
+
+        iPack = nbp-1;
+        MPI_Status status;
+
+        // On parcourt tous les packs
+        while(iPack<N){
+            //On recupere le message de n'importe quelle source
+            MPI_Recv(buffer.data(), N, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, globComm,&status);
+
+            // On recupere le rang de la source
+            int slaveRk = status.MPI_SOURCE;
+
+            // On somme dans le vecteur final :
+            somme(result, buffer, N);
+            // On envoie un message à cette source
+            MPI_Send(&iPack, 1, MPI_INT, slaveRk, 101, globComm);
+            iPack ++;
+            
+        }
+        iPack = -1;
+
+        // Pour tous les processus : 
+        for(int i=1; i<nbp; i++){
+            //On recupere le message de n'importe quelle source
+            MPI_Recv(buffer.data(), N, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, globComm,&status);
+
+            // On recupere le rang de la source
+            int slaveRk = status.MPI_SOURCE;
+
+            // On somme dans le vecteur final :
+            result = somme(result, buffer, N);
+
+            // On envoie un message à cette source
+            MPI_Send(&iPack, 1, MPI_INT, slaveRk, 101, globComm);
+        }
+
+         // A la fin, on test le resultat :
+        std::cout  << "A : " << A << std::endl;
+        std::cout << " u : " << u << std::endl;
+        std::cout<<"\nTest du resultat :\n"<<std::endl;
+        std::vector<double> v = A*u;
+        std::cout<<"Produit classique : "<< v <<"\n"<<std::endl;
+        std::cout<<"Produit parallelisé : "<<result<<"\n"<<std::endl;
+
+        if(v==result){
+            std::cout<<"\nTest validé\n"<<std::endl;
+        }
+        else{
+            std::cout<<"\nTest non validé\n"<<std::endl;
+        }
+
+
+
+
+    }
+
+    // Esclaves :
+    else{
+        MPI_Status status;
+        int iPack;
+        do{
+            MPI_Recv(&iPack, 1, MPI_INT, 0, 101, globComm, &status);
+            if(iPack!=-1){
+                // Do
+                // Produit de la colonne A_iPack avec u:
+                prod(A, iPack, u, buffer.data(), N);
+
+
+                // Renvoyer une info (numero de ligne du vecteur) en TAG
+                MPI_Send(buffer.data(), N, MPI_DOUBLE, 0, iPack,globComm);
+            }
+        }while(iPack!=-1);
+    }
+
+
+
+    MPI_Finalize();
+    
     return EXIT_SUCCESS;
 }
